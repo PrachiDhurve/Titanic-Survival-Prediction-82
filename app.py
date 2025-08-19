1ef7b0945a22119948f2fc0f901ac471bca56d40
Prachi <prachidhurve2001@gmail.com>
2025-08-20 00:18:36 +0530

    removing import-not needed

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import numpy as np 
 import pandas as pd 
-import shap
 import matplotlib.pyplot as plt
 
 st.set_page_config(page_title="Titanic Survival Prediction", layout="centered")
